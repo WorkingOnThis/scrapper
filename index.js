0cd6100a209b1f9bfdc67b6f554d5990d8716e2c
Demián S. Torres <demiansamirtorres@gmail.com>
2022-11-12 23:11:40 -0300

    console duplicado

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ import puppeteer from "puppeteer";
   const pasadoANumero = Number(resultadoValorDolar.replace("$", ""));
 
   console.log(pasadoANumero);
+  console.log(pasadoANumero);
   // // Type into search box.
   // await page.type(".devsite-search-field", "Headless Chrome");
 
